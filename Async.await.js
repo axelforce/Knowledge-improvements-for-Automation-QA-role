9581b4176848116ebd9ba99f4d01c09f796da45a
axelforce <oleksandr.kulagin@gmail.com>
2022-07-11 06:47:57 +0300

    added results

--- Async.await.js
+++ Async.await.js
@@ -31,3 +31,17 @@ const asyncFn3 = async () => {
 asyncFn3();
 //________________________________________________
 
+const getData = async (url) => {
+  const res = await fetch(url);
+  const json = await res.json();
+  return json
+  }
+
+const url = 'https://jsonplaceholder.typicode.com/todos/1';
+
+try {
+  const data = await getData(url);
+  console.log(data);
+} catch (error) {
+  console.log(error.message);
+}
