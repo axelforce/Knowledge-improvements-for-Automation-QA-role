90c1f9f207af03a716cbaf508c11cc503d045216
axelforce <oleksandr.kulagin@gmail.com>
2022-07-21 14:22:24 +0300

    add promise

--- Promises.js
+++ Promises.js
@@ -1,40 +1,17 @@
-const myPromise = new Promise((resolve, reject) => {
-
-})
-
-myPromise
-  .then (value => {
-
-  })
-  .catch(error => {
-
-  })
-//_____________________________________________
-
-fetch('https://jsonplaceholder.typicode.com/todos')
-  .then(response => response.json())
-  .then(json => console.log(json))
-  .catch(error => console.error(error))
-//_____________________________________________
-
-fetch('https://jsonplaceholder.typicode.com/todos')
-  .then(response => {
-    console.log(response)
-    return response.json()
-  })
-  .then(json => console.log(json))
-  .catch(error => console.error(error))
-//_____________________________________________
-
-const getData = (url) =>
-  new Promise ((resolve, reject) => {
-    fetch(url)
-      .then(response => response.json())
-      .then(json => console.log(json))
-      .catch(error => reject(error))
-})
-
-getData('https://jsonplaceholder.typicode.com/todos')
-  .then(data => console.log(data))
-  .catch(error => console.log(error.message))
-//_____________________________________________
+const clickAndWaitElement = () =>
+  new Promise((resolve, reject) => {
+    const interval = setInterval(() => {
+      const loginButton = document.getElementById('login-button').click();
+      const errorMessage = document.querySelectorAll('div.error-message-container');
+      if (errorMessage.length > 0 || loginButton != null) {
+        resolve('Element is present')
+        clearInterval(interval);
+      }
+      console.log('click and find element')
+    }, 300)
+    setTimeout(() => {
+      reject("Timeout: element not found");
+      clearInterval(interval);
+    }, 10000);
+  });
+clickAndWaitElement();
